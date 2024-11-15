b3027ae57d3852e1c6f030b2ab7e0ba7de492909
Moligarch <mr.verkiani@gmail.com>
2024-11-15 16:48:22 +0330

    Initialize Project

---
+++ resource.h
@@ -0,0 +1,21 @@
+	//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by HashInHandLib.rc
+
+#define VER_PRODUCTVERSION          0,1,0
+#define VER_PRODUCTVERSION_STR      "0.1.0\0"
+
+#define COMPANY_STR                 "Moein Verkiani (@Moligarch)"
+#define COPYRIGHT_STR               "Copyright (C) 2024 Moein Verkiani (@Moligarch)"
+
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        101
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
