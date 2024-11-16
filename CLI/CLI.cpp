cf19fb60e04e18a095a5c904df533e9eaf488298
Moein Verkiani <mr.verkiani@gmail.com>
2024-11-16 19:04:21 +0330

    Testing Layer by Layer parsing

--- CLI/CLI.cpp
+++ CLI/CLI.cpp
@@ -1,16 +1,19 @@
 #include <iostream>
-#include <pcapplusplus/IPv4Layer.h>
-#include <pcapplusplus/IPv6Layer.h>
-#include <pcapplusplus/EthLayer.h>
-#include <pcapplusplus/Packet.h>
 #include <pcapplusplus/PcapFileDevice.h>
+#include <pcapplusplus/Packet.h>
+#include <pcapplusplus/EthLayer.h>
+#include <pcapplusplus/IPv4Layer.h>
+#include <pcapplusplus/TcpLayer.h>
+#include <pcapplusplus/HttpLayer.h>
 #include <unordered_map>
 #include <vector>
 
 int main(int argc, char* argv[])
 {
     // open a pcap file for reading
-    std::string pcapPath{ "D:/Work/Projects/Windows/PcapPatrol/Samples/2019-01-28-traffic-analysis-exercise.pcap" };
+    std::string pcapPath;
+    std::cout << "Enter pcap file path: ";
+    std::getline(std::cin, pcapPath);
     pcpp::PcapFileReaderDevice reader(pcapPath);
     if (!reader.open())
     {
@@ -19,96 +22,86 @@ int main(int argc, char* argv[])
     }
 
     // create maps to store IP addresses and their repetition counts
-    std::unordered_map<std::string, int> ipv4Counts;
-    std::unordered_map<std::string, int> ipv6Counts;
-    std::unordered_map<std::string, int> ethCounts;
+    //std::unordered_map<std::string, int> ipv4SrcCounts;
+    //std::unordered_map<std::string, int> ipv4DstCounts;
+
+    //std::unordered_map<std::string, int> ipv6SrcCounts;
+    //std::unordered_map<std::string, int> ipv6DstCounts;
+    //std::unordered_map<std::string, int> ethSrcCounts;
+    //std::unordered_map<std::string, int> ethDstCounts;
+
     int nPacket{ 0 };
     // read packets from the pcap file
     pcpp::RawPacket rawPacket;
     do {
         if (!reader.getNextPacket(rawPacket))
         {
-            std::cerr << "Couldn't read the next packet in the file" << std::endl;
+            std::cerr << "Reading pcap file finished." << std::endl;
             break;
         }
-
         nPacket++;
+
         // parse the raw packet into a parsed packet
         pcpp::Packet parsedPacket(&rawPacket);
 
-        //std::cout << "Packet info:\n"
-        //    << parsedPacket.toString() << std::endl;
+        pcpp::EthLayer* l1 = parsedPacket.getLayerOfType<pcpp::EthLayer>();
+        auto l2 = dynamic_cast<pcpp::IPv4Layer * >(l1->getNextLayer());
+        auto l3 = dynamic_cast<pcpp::TcpLayer*>(l2->getNextLayer());
+        //auto l4_req = dynamic_cast<pcpp::HttpRequestLayer*>(l3->getNextLayer());
+        //auto l4_res = dynamic_cast<pcpp::HttpResponseLayer*>(l3->getNextLayer());
 
-        // check if the packet is IPv4
-        if (parsedPacket.isPacketOfType(pcpp::IPv4))
-        {
-            // extract source and destination IPs for IPv4
-            pcpp::IPv4Layer* ipv4Layer = parsedPacket.getLayerOfType<pcpp::IPv4Layer>();
-            pcpp::IPv4Address srcIP = ipv4Layer->getSrcIPv4Address();
-            pcpp::IPv4Address destIP = ipv4Layer->getDstIPv4Address();
+        std::string srcMac = l1->getSourceMac().toString();
+        std::string destMac = l1->getDestMac().toString();
 
-            // convert IPv4 addresses to string
-            std::string srcIPStr = srcIP.toString();
-            std::string destIPStr = destIP.toString();
-
-            // increment the count of the IPv4 addresses
-            ipv4Counts[srcIPStr]++;
-            ipv4Counts[destIPStr]++;
-        }
-        // check if the packet is IPv6
-        else if (parsedPacket.isPacketOfType(pcpp::IPv6))
-        {
-            // extract source and destination IPs for IPv6
-            pcpp::IPv6Layer* ipv6Layer = parsedPacket.getLayerOfType<pcpp::IPv6Layer>();
-            pcpp::IPv6Address srcIP = ipv6Layer->getSrcIPv6Address();
-            pcpp::IPv6Address destIP = ipv6Layer->getDstIPv6Address();
-
-            // convert IPv6 addresses to string
-            std::string srcIPStr = srcIP.toString();
-            std::string destIPStr = destIP.toString();
-
-            // increment the count of the IPv6 addresses
-            ipv6Counts[srcIPStr]++;
-            ipv6Counts[destIPStr]++;
-        }
-        else if (parsedPacket.isPacketOfType(pcpp::Ethernet))
-        {
-            pcpp::EthLayer* ethLayer = parsedPacket.getLayerOfType< pcpp::EthLayer>();
-            std::string srcMAC = ethLayer->getSourceMac().toString();
-            std::string dstMAC = ethLayer->getDestMac().toString();
-            ethCounts[srcMAC]++;
-            ethCounts[dstMAC]++;
-        }
+        std::string srcIPv4 = l2->getSrcIPv4Address().toString();
+        std::string destIPv4 = l2->getDstIPv4Address().toString();
+        
     } while (true);
 
     // close the pcap file
     reader.close();
 
     // Output the counts of IP addresses
-    if (!ipv4Counts.empty())
+    //if (!ipv4SrcCounts.empty() && !ipv4DstCounts.empty())
+    //{
+    //    std::cout << "IPv4 Source Address Counts:" << std::endl;
+    //    for (const auto& entry : ipv4SrcCounts) {
+    //        std::cout << entry.first << " : " << entry.second << std::endl;
+    //    }
+
+    //    std::cout << "IPv4 Destination Address Counts:" << std::endl;
+    //    for (const auto& entry : ipv4DstCounts) {
+    //        std::cout << entry.first << " : " << entry.second << std::endl;
+    //    }
+    //}
+
+    /*if (!ipv6SrcCounts.empty() && !ipv6DstCounts.empty())
     {
-        std::cout << "IPv4 Address Counts:" << std::endl;
-        for (const auto& entry : ipv4Counts) {
+        std::cout << "IPv6 Source Address Counts:" << std::endl;
+        for (const auto& entry : ipv6SrcCounts) {
+            std::cout << entry.first << " : " << entry.second << std::endl;
+        }
+
+        std::cout << "IPv6 Destination Address Counts:" << std::endl;
+        for (const auto& entry : ipv6DstCounts) {
             std::cout << entry.first << " : " << entry.second << std::endl;
         }
     }
 
-    if (!ipv6Counts.empty())
+    if (!ethSrcCounts.empty() && !ipv4DstCounts.empty())
     {
-        std::cout << "IPv6 Address Counts:" << std::endl;
-        for (const auto& entry : ipv6Counts) {
+        std::cout << "Eth Source Address Counts:" << std::endl;
+        for (const auto& entry : ethSrcCounts) {
             std::cout << entry.first << " : " << entry.second << std::endl;
         }
-    }
 
-    if (!ethCounts.empty()) 
-    {
-        std::cout << "Eth Address Counts:" << std::endl;
-        for (const auto& entry : ethCounts) {
+        std::cout << "Eth Destination Address Counts:" << std::endl;
+        for (const auto& entry : ethDstCounts) {
             std::cout << entry.first << " : " << entry.second << std::endl;
         }
-    }
+    }*/
 
     std::cout << "Total Number of Packet: " << nPacket << std::endl;
+
     return 0;
 }
